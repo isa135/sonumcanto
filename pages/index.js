6d932f4c2191f627ea61b20eda544fdc61b819e7
Isa Accessor <isa.accessor@gmail.com>
2021-03-12 17:44:45 -0300

    Página Teste Inicial

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Olá! Tudo bem?</h1>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+    
+    function adicionaContador() {
+        setContador(contador+1);
+    }
+    
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionaContador}>Adiciona</button>
+        </div>
+    )
+}
+export default Home
