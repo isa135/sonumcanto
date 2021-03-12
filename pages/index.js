9e9e272aa0673165220b806a3f6ea4672ae12932
Isa Accessor <isa135@gmail.com>
2021-03-12 18:30:08 -0300

    Teste 2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 function Home() {
     return (
         <div>
-            <h1>Ambiente de teste</h1>
+            <h1>Ambiente de teste Blá Blá Blá</h1>
             {/* <Contador /> */}
         </div>
     )
